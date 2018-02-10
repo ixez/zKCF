87203e24545963e7687cf41869973431be2c9c0b
Zeke <huzexi@outlook.com>
2018-02-10 21:36:49 +0800

    Add gflags dependency

--- src/FkFactory.cpp
+++ src/FkFactory.cpp
@@ -5,6 +5,12 @@ Email: sachika.misawa@outlook.com
 */
 #include "Features/VggFeature.h"
 #include "FkFactory.h"
+
+DEFINE_string(prototxt, "VGG_ILSVRC_16_layers_deploy.prototxt.txt", "Prototxt file of the net");
+DEFINE_string(caffemodel, "VGG_ILSVRC_16_layers.caffemodel", "Caffemodel file of the net");
+DEFINE_string(layer, "conv5_1", "Layer to output feature maps");
+DEFINE_string(meanproto, "imagenet_mean.binaryproto", "Binaryproto file of the net");
+
 namespace zkcf {
     void FkFactory(FeatureType ft, KernelType kt, IFeature *&f, IKernel *&k) {
         switch (ft) {
@@ -22,12 +28,7 @@ namespace zkcf {
                 break;
 #ifdef BUILD_VGG
             case FEAT_VGG:
-                f = new VggFeature("/home/zeke/Codes/zKCF/assets/vgg/VGG_ILSVRC_16_layers_deploy.prototxt.txt",
-                                   "/home/zeke/Codes/zKCF/assets/vgg/VGG_ILSVRC_16_layers.caffemodel",
-                                   "conv5_1",
-                                   "/home/zeke/Libs/caffe/data/ilsvrc12/imagenet_mean.binaryproto",
-                                   nullptr
-                );
+                f = new VggFeature(FLAGS_prototxt, FLAGS_caffemodel, FLAGS_layer, FLAGS_meanproto, nullptr);
 //                f = new VggFeature("./assets/vgg/VGG_CNN_M_2048_deploy.prototxt",
 //                                   "./assets/vgg/VGG_CNN_M_2048.caffemodel",
 //                                   "conv1"
